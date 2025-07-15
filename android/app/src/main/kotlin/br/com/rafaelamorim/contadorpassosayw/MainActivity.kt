24a2a4bb5518f65f92b401441c606f232900fba6
WadeWatts <103043814+WadeWatts9@users.noreply.github.com>
2025-07-14 22:58:10 -0300

    se modifica datos referentes al paquete.

---
+++ android/app/src/main/kotlin/br/com/rafaelamorim/contadorpassosayw/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.rafaelamorim.contadorpassosayw
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- android/app/src/main/kotlin/com/example/contador_pasos/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.contador_pasos
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
